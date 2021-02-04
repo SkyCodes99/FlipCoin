67b8259ea48f79419b7656ebf03054a475342e8a
Sky <31568208+SkyCodes99@users.noreply.github.com>
2021-02-04 07:02:08 -0500

    Add project files.

---
+++ FlipCoin/CoinFlip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class CoinFlip
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hit a key to flip a coin");
+                if(Console.ReadKey(true).Key != ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                }
+                Random rnd = new Random();
+                int coin = rnd.Next(1, 3);
+                string side = " ";
+                switch (coin)
+                {
+                    case 1:
+                        side = "Heads";
+                        break;
+                    case 2:
+                        side = "Tails";
+                        break;
+                }
+                Console.WriteLine(side);
+                Console.WriteLine("Flip Again?");
+                string response = Console.ReadLine();
+                if(response.ToLowerInvariant() == "no")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
